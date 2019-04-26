343d387acb4d6147757745b057fbfc18ba23334c
Iryna Koroliuk <irene50288@gmail.com>
2019-04-26 13:39:43 -0700

    fixes requested in comments to the PR

--- CatalogPage.js
+++ CatalogPage.js
@@ -2,80 +2,70 @@ import React, { Component } from 'react';
 import Products from './constants/Products';
 
 class Image extends Component {
-    constructor(props) {
-        super(props);
-    }
 
-    render() {
-        const product = this.props.product;
-        return (
-            <img src={product.imageUrl} width='100' height='100' alt={product.title}/>
-        )
-    }
+  render() {
+    const { imageUrl, alt } = this.props;
+    return (
+      <img src={imageUrl} width='100' height='100' alt={alt}/>
+    )
+  }
 }
 
 class TextBox extends Component {
-    constructor(props) {
-        super(props);
-    }
 
-    render() {
-        const product = this.props.product;
-        return (
-            <span>{product.title}</span>
-        )
-    }
+  render() {
+    const text = this.props.text;
+    return (
+      <span>{text}</span>
+    )
+  }
 }
 
 class Price extends Component {
-    constructor(props) {
-        super(props);
-    }
 
-    render(){
-        const product = this.props.product;
-        return <span>{product.price}</span>
-    }
+  render(){
+    const price = this.props.price;
+    return <span>${price}</span>
+  }
 }
 
 class ProductCard extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        const product = this.props.product;
-        return (
-            <div>
-                <TextBox product={product}/>
-                <Price product={product}/>
-                <Image product={product}/>
-            </div>
-        )
-    }
+
+  render() {
+    const product = this.props.product;
+    return (
+      <div>
+        <TextBox text={product.title}/>
+        <Price price={product.price}/>
+        <Image imageUrl={product.imageUrl} al={product.title}/>
+      </div>
+    )
+  }
 }
 
 class Catalog extends Component {
-    render() {
-        const { products } = this.props;
-        return (
-            <ul>
-                {
-                    products.map(product => {
-                        return <li key = {product.id}><ProductCard product={product}/></li>
-                    })
-                }
-            </ul>
-        )
-    }
+
+  render() {
+    const { products } = this.props;
+    return (
+      <ul>
+        {
+          products.map(product => {
+            return <li key = {product.id}><ProductCard product={product}/></li>
+          })
+        }
+      </ul>
+    )
+  }
 }
 
 class CatalogPage extends Component {
-    render() {
-        const products = Products;
-        return (
-            <Catalog products={products}/>
-        )
-    }
+  render() {
+    const products = Products;
+    return (
+      <Catalog products={products}/>
+    )
+  }
 
 }
 
